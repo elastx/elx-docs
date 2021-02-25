beb869fe01eead48b027b99047a15bcf6ff7c237
Andreas Holmsten <andreas.holmsten@elastx.se>
2021-02-25 09:33:20 +0100

    Add /docs to echoed url

--- test-locally.sh
+++ test-locally.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-echo "Building and serving web content on http://localhost:1313 (Ctrl + C to exit)"
+echo "Building and serving web content on http://localhost:1313/docs (Ctrl + C to exit)"
 git submodule update --init --recursive
 docker run --rm -v $(pwd):/root/project/ -p 1313:1313 -e HUGO_ENV=production quay.io/elastx/ci-hugo:0.74.3 hugo server --bind 0.0.0.0
