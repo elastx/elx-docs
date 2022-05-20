4afc24743624854b0ef95724f43c3b2bc2b88887
Hugo Blom <hugo@blommail.com>
2022-05-20 09:47:15 +0200

    update hugo to 0.99.1

--- test-locally.sh
+++ test-locally.sh
@@ -2,4 +2,4 @@
 
 echo "Building and serving web content on http://localhost:1313/docs (Ctrl + C to exit)"
 git submodule update --init --recursive
-docker run --rm -v $(pwd):/root/project/ -p 1313:1313 -e HUGO_ENV=production quay.io/elastx/ci-hugo:0.87.0 hugo server --bind 0.0.0.0
+docker run --rm -v $(pwd):/root/project/ -p 1313:1313 -e HUGO_ENV=production quay.io/elastx/ci-hugo:v0.99.1 hugo server --bind 0.0.0.0
