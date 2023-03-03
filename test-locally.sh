9d8f90e8258b50671f40153c29346dc5e0953900
Andreas Holmsten <andreas.holmsten@elastx.se>
2023-03-03 09:55:52 +0100

    Update to Hugo 0.110.0, Docsy 0.6.0

--- test-locally.sh
+++ test-locally.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
 
 echo "Building and serving web content on http://localhost:1313/docs (Ctrl + C to exit)"
-git submodule update --init --recursive
-docker run --rm -v $(pwd):/root/project/ -p 1313:1313 -e HUGO_ENV=production quay.io/elastx/ci-hugo:v0.99.1 hugo server --bind 0.0.0.0
+docker run --rm -v $(pwd):/root/project/ -p 1313:1313 -e HUGO_ENV=production quay.io/elastx/ci-hugo:v0.110.0 hugo server --bind 0.0.0.0
